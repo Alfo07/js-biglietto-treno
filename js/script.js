bcc82b210983067a388d5c44540bb686c47a2bc1
Alfo07 <ammiratialfonso07@gmail.com>
2022-03-24 14:33:55 +0100

    Ticket train

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //Numero di chilometri
 let chilometri = parseInt(prompt('Quanti chilometri deve percorrere?'));
-console.log(chilometri);
+console.log('Chilometri che il passeggero deve percorrere: ' + chilometri);
 
 //Età del passeggero
 let userAge = parseInt(prompt('Quanti anni ha?'));
-console.log(userAge);
+console.log('Età del passeggero: ' + userAge);
 
 //Prezzo intero del biglietto 
 let fullPrice = (chilometri * 0.21);
@@ -12,12 +12,12 @@ let fullPrice = (chilometri * 0.21);
 //Prezzo scontato
 if (userAge < 18){
     let under18 = (fullPrice - ((fullPrice * 20) / 100));
-    console.log(under18.toFixed(2) + '€');
+    console.log('Prezzo del biglietto scontato con la tariffa Under 18: ' + under18.toFixed(2) + '€');
 }
 else if(userAge > 65){
     let over65 = (fullPrice - ((fullPrice * 40) / 100));
-    console.log(over65.toFixed(2) + '€');
+    console.log('Prezzo del biglietto scontato con la tariffa Over 65: ' + over65.toFixed(2) + '€');
 }
 else{
-    console.log(fullPrice + '€');
+    console.log('Prezzo intero del biglietto: ' + fullPrice + '€');
 }
